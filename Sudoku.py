f7c3eb15c3606d506c16c63459ce72925816ce87
KurtClarkKieferRomero <mrdmdarius215@gmail.com>
2022-11-19 17:42:14 +0800

    Visual Representation for the Board

--- Sudoku.py
+++ Sudoku.py
@@ -12,3 +12,18 @@ sudoku_board= [
     [4, 9, 2, 0, 0, 0, 0, 0, 0]
 ]
 # 0 meant blank space
+# Visual representation for the sudoku board
+def print_board(bo):
+    for i in range(len(bo)):
+        if i % 3 == 0 and i != 0:
+            print("- - - - - - - - - - - -")
+
+        for j in range (len(bo[0])):
+            if j % 3 == 0 and j != 0:
+                print(" | ", end="")
+
+            if j == 8:
+                print(bo[i][j])
+            else:
+                print(str(bo[i][j]) + " ", end="")
+
